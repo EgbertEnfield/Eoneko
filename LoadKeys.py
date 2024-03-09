a7bb5ae2c540b3d07c02a0aa7cd599f2fde23021
EgbertEnfield <muska5312187@gmail.com>
2024-03-10 02:26:12 +0900

    tested bot parrots

--- LoadKeys.py
+++ LoadKeys.py
@@ -2,19 +2,30 @@ import json
 import os
 
 ID_FILE_NAME = "id.json"
+CID_FILE_NAME = "cid.json"
 
 
-def LoadKeysFromFile(fname=ID_FILE_NAME):
+def _LoadKeysFromFile(fname=ID_FILE_NAME):
     f = open(fname, 'r', encoding="UTF-8")
     return json.load(f)
 
 
-def LoadKeysFromEnv():
+def _LoadKeysFromEnv():
     return {"APP_ID": os.environ["APP_ID"], "PUB_KEY": os.environ["PUB_KEY"], "TOKEN": os.environ["TOKEN"]}
+
+
+def _LoadChannelIDFromFile(fname="cid.json"):
+    f = open(fname, 'r', encoding="UTF-8")
+    return json.load(f)
 
 
 def GetDiscordKeys():
     if os.path.isfile(ID_FILE_NAME):
-        return LoadKeysFromFile()
+        return _LoadKeysFromFile()
     else:
-        return LoadKeysFromEnv()
+        return _LoadKeysFromEnv()
+
+
+def GetDiscordChannelID():
+    if os.path.isfile(CID_FILE_NAME):
+        return _LoadChannelIDFromFile()
--- disc_main.py
+++ disc_main.py
@@ -1,24 +1,37 @@
 import discord
 from LoadKeys import GetDiscordKeys
+from LoadKeys import GetDiscordChannelID
+
+DEBUG = True
+
+Keys = GetDiscordKeys()
+Channels = GetDiscordChannelID()
 
 
 class DiscordMain(discord.Client):
     async def on_ready(self):
         print("logged in")
-        return
 
     async def on_message(self, message):
-        return
+        print(f"Received:\n{message.author}: {message.content}")
+        if message.author == self.user:
+            return
+
+        if DEBUG:
+            if message.channel.id == int(Channels["CHANNEL"]["TEST"]):
+                await message.channel.send(message.content)
+            else:
+                return
+        else:
+            await message.channel.send(message.content)
 
 
-def main():
-    keys = GetDiscordKeys()
+def Main():
     intents = discord.Intents.default()
     intents.message_content = True
     client = DiscordMain(intents=intents)
-    client.run(keys["TOKEN"])
-    return
+    client.run(Keys["TOKEN"])
 
 
 if __name__ == "__main__":
-    main()
+    Main()
