0523b2e0875d3ad14eb69702355f4a6acf46fa95
EgbertEnfield <muska5312187@gmail.com>
2024-03-10 01:42:58 +0900

    add login method

--- LoadKeys.py
+++ LoadKeys.py
@@ -5,16 +5,16 @@ ID_FILE_NAME = "id.json"
 
 
 def LoadKeysFromFile(fname=ID_FILE_NAME):
-  f = open(fname, 'r', encoding="UTF-8")
-  return json.load(f)
+    f = open(fname, 'r', encoding="UTF-8")
+    return json.load(f)
 
 
 def LoadKeysFromEnv():
-  return {"APP_ID": os.environ["APP_ID"], "PUB_KEY": os.environ["PUB_KEY"]}
+    return {"APP_ID": os.environ["APP_ID"], "PUB_KEY": os.environ["PUB_KEY"], "TOKEN": os.environ["TOKEN"]}
 
 
 def GetDiscordKeys():
-  if os.path.isfile(ID_FILE_NAME):
-    return LoadKeysFromFile()
-  else:
-    return LoadKeysFromEnv()
+    if os.path.isfile(ID_FILE_NAME):
+        return LoadKeysFromFile()
+    else:
+        return LoadKeysFromEnv()
--- disc_main.py
+++ disc_main.py
@@ -1,8 +1,22 @@
 import discord
-import json
+from LoadKeys import GetDiscordKeys
+
+
+class DiscordMain(discord.Client):
+    async def on_ready(self):
+        print("logged in")
+        return
+
+    async def on_message(self, message):
+        return
 
 
 def main():
+    keys = GetDiscordKeys()
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = DiscordMain(intents=intents)
+    client.run(keys["TOKEN"])
     return
 
 
