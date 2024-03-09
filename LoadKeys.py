98fdf87fe52b94be88c7f0e8739c60fc41feba8b
Egbert Enfield <77866829+EgbertEnfield@users.noreply.github.com>
2024-03-09 15:32:01 +0000

    Add getting discord key method both from Replit or my *safiscated PC

--- LoadKeys.py
+++ LoadKeys.py
@@ -1,7 +1,20 @@
 import json
 import os
 
+ID_FILE_NAME = "id.json"
 
-def LoadKeys(fname="id.json"):
-    f = open(fname, 'r', encoding="UTF-8")
-    return json.load(f)
+
+def LoadKeysFromFile(fname=ID_FILE_NAME):
+  f = open(fname, 'r', encoding="UTF-8")
+  return json.load(f)
+
+
+def LoadKeysFromEnv():
+  return {"APP_ID": os.environ["APP_ID"], "PUB_KEY": os.environ["PUB_KEY"]}
+
+
+def GetDiscordKeys():
+  if os.path.isfile(ID_FILE_NAME):
+    return LoadKeysFromFile()
+  else:
+    return LoadKeysFromEnv()
